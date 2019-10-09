1d51039aec19a7be642a98f8f0c4c38d60bd502f
hzy <1109007763@qq.com>
2019-10-09 16:11:51 +0800

    添加富文本组件

---
+++ html/index.js
@@ -0,0 +1,29 @@
+// components/html/index.js
+const wxParse = require('../../wxParse/wxParse.js');
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        data:{
+            type: String,
+            observer(newVal, oldVal) {
+                // 属性值变化时执行
+                wxParse.wxParse('article', 'html', newVal, this, 5);
+            }
+        }
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
