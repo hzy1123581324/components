014c322412a96c9cac5bc394d4a8295f998dd648
hzy <1109007763@qq.com>
2019-10-30 14:47:33 +0800

    不能重复点击按钮组件

---
+++ postpone.vue
@@ -0,0 +1,70 @@
+<template>
+	<view :class="['btn',!canclick&&'active']" hover-class="none" @click="__change"><slot>确定</slot></view>
+</template>
+
+<script>
+	export default {
+		props:{
+			tap:{
+				type: Function,
+			},
+		},
+		data() {
+			return {
+				canclick: true,
+			}
+		},
+		onLoad() {
+	
+		},
+		methods: {
+			__tap(){
+				return new Promise((resolve, reject)=> {
+					this.tap(resolve,reject)	
+				})
+			},
+			__change(){
+				let {canclick} = this;
+				console.log('&&&&&&&&&&&&&')
+				if(canclick){
+					if(this.tap){
+						this.canclick = false;
+						this.__tap().then(()=>{
+							console.log(4444)
+							this.canclick = true;
+						}).catch(()=>{
+							console.log(5555)
+							this.canclick = true;
+						});
+					}else{
+						uni.showToast({
+							title: '还没有设置方法',
+							icon: 'none'
+						})
+					}
+				}else{
+					uni.showToast({
+						title: '请不要重复点击',
+						icon: 'none'
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped lang="stylus">
+	.btn
+		width auto
+		
+		margin 0 30upx
+		height 80upx
+		text-align center
+		line-height 80upx
+		color #FFFFFF
+		background-color #f55
+		font-size 32upx
+		border-radius 10upx
+	&.active
+		opacity 0.4
+</style>
