796d67153fac078aa0b698cbfd9314615907860c
hzy <1109007763@qq.com>
2020-01-06 19:09:08 +0800

    添加新组件

---
+++ noData.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="">
+		<solt>{{'~~'+i18n.nodata+'~~'}}</solt>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ scrollMulti.vue
@@ -0,0 +1,16 @@
+<!-- 
+滑动组件功能，
+ 下拉刷新
+ 上拉加载更多
+ 吸顶
+ 
+ -->
+
+<template>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
