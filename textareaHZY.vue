7ea19eaf9d97510b0837323b506f618da0b34ab9
hzy <1109007763@qq.com>
2020-01-17 14:40:51 +0800

    修复

---
+++ textareaHZY.vue
@@ -0,0 +1,52 @@
+<template>
+	<view id="text-area-hzy">
+		<textarea :class="['test-area-into',classStyle]" v-model="value" auto-focus="true" wx:if="showTextarea" @blur="textareaBlur" :placeholder="placeholder" />
+		<view :class="['test-area-view',classStyle]" @tap="showTextarea = true">{{value||placeholder}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+			props: {
+				value: {
+					type: String,
+					default: '',
+				},
+				placeholder:{
+					type: String,
+					default: ''
+				},
+				classStyle: {
+					
+				}
+				
+			},
+			data() {
+				return {
+					showTextarea: false,
+				}
+			},
+			onLoad() {
+	
+			},
+			methods: {
+				textareaBlur(){
+					this.showTextarea = false;
+				}
+			},
+			watch: {
+	
+			},
+			computed: {
+	
+			}
+		}
+	
+</script>
+
+<style>
+	.test-area-into,.test-area-view{
+		width: 100%;
+		height: 100%;
+	}
+</style>
