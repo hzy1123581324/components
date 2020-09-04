d00905b1de8ed322ee0dd6a91defc1a0daf18b55
hzy <1109007763@qq.com>
2020-09-04 18:17:36 +0800

    修改postpone中的staytime异常

--- chat/chat.vue
+++ chat/chat.vue
@@ -1,18 +1,27 @@
 <template>
   <view class="chat-box">
     <!-- 聊天室主体 -->
-    <view>
-      <view v-for="item in chatRecord" :key="item.id"></view>
+    <view class="chat-title">某某某</view>
+    <view class="chat-main">
+        <scroll-view scroll-y="true" class="chat-main-scroll">
+            
+            <view v-for="item in chatRecord" :key="item.id"></view>
+        </scroll-view>
     </view>
     <!-- 输入框 -->
-    <view></view>
+    <view class="chat-input-area">
+        <view class="input-area" contenteditable="true" >
+            
+        </view>
+        <view class="send-btn">发送</view>
+    </view>
   </view>
 </template>
 
 <script>
 /**
  * counter 聊天室
- * @description 商品自增自减
+ * @description 自定义聊天室
  * @tutorial https://www.uviewui.com/components/button.html
  * @property {String,Number} time 倒计时时间
  * @example <chat></chat>
@@ -41,5 +50,40 @@ export default {
 };
 </script>
 <style scoped >
+    .chat-box{
+        width: 100%;
+        height: 100%;
+        min-height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .chat-title{
+        text-align: center;
+        line-height: 76upx;
+        font-size: 32upx;
+        font-weight: 600;
+    }
+    .chat-main{
+        flex-grow: 1;
+        height: 50vh;
+    }
+    .chat-main-scroll{
+        max-height: 100%;
+    }
+    .chat-input-area,.chat-title{
+        flex-shrink: 0;
+    }
+    
+    .chat-input-area{
+        display: flex;
+        height: 80upx;
+        white-space: pre-wrap;
+        
+    }
+    .input-area{
+        /* contenteditable:true; */
+        flex-grow: 1;
+        
+    }
 </style>
 
