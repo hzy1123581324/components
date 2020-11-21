40752b2e3368d4490d14ec967c740a3c99e44599
hzy <1109007763@qq.com>
2020-11-21 15:17:08 +0800

    新增助记词组件

---
+++ mnemonic/mnemonic.vue
@@ -0,0 +1,25 @@
+<template>
+    <view class="">
+        
+    </view>
+</template>
+
+<script>
+    export default{
+        name: 'mnemonic',
+        data: ()=>{
+            return {
+                
+            }
+        },
+        mounted(){
+            
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style>
+</style>
